d72cdce5e677dc9fd0e7d869c7f96c77e857702f
Salan-28 <salan_btechedit2023@kusoed.edu.np>
2024-11-08 15:21:36 +0545

    completed_task3 calculator

---
+++ calculator.py
@@ -0,0 +1,43 @@
+def calculator():
+        
+    print("Select one operation:")
+    print("1. Addition:")
+    print("2. Subtraction:")
+    print("3. Multiplication:")
+    print("4. Division:")
+    print("5. Modulus:")
+
+    def add(a,b):
+        return a+b
+    def sub(a,b):
+        return a-b
+    def mult(a,b):
+        return a*b
+    def div(a,b):
+        return a/b
+    def mod(a,b):
+        return a % b
+    while True:
+            choice = input("Enter choice (1/2/3/4/5) or 'exit' to quit: ")
+            
+            if choice.lower() == 'exit':
+                print("Exiting the calculator.")
+                break
+            
+            if choice in ['1', '2', '3', '4','5']:
+                    num1 = float(input("Enter first number: "))
+                    num2 = float(input("Enter second number: "))
+                    
+                    if choice == '1':
+                        print(f"Result: {add(num1, num2)}")
+                    elif choice == '2':
+                        print(f"Result: {sub(num1, num2)}")
+                    elif choice == '3':
+                        print(f"Result: {mult(num1, num2)}")
+                    elif choice == '4':
+                        print(f"Result: {div(num1, num2)}")
+                    elif choice == '5':
+                        print(f"Result: {mod(num1, num2)}")
+            else:
+                print("Invalid choice. Please select a valid operation.")
+calculator()
